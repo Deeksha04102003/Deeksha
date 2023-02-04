ddfd31dfb255f51185ffeee4bac0ad1329924917
Deeksha04102003 <dishacronical0blr@gmail.com>
2023-02-04 17:12:06 +0530

    My second commit

---
+++ Deekshaa.java
@@ -0,0 +1,8 @@
+Public Class Addition
+     Public static void main(string[] args)
+     {
+        int a=5;
+        int b=2;
+        int sum = a + b ;
+        system.out.println("Addition is" +sum);
+     }
