d5f44abfbff45e9d690e1b36577b348cd6f74cd4
Deeksha04102003 <dishacronical0blr@gmail.com>
2023-02-16 12:13:58 +0530

    SECOND COMMIT

--- Deekshaa.java
+++ Deekshaa.java
@@ -1,8 +1,8 @@
-Public Class Addition
+Public Class Subtraction 
      Public static void main(string[] args)
      {
         int a=5;
         int b=2;
-        int sum = a + b ;
-        system.out.println("Addition is" +sum);
+        int Diff = a - b ;
+        system.out.println("Subtraction is" +sum);
      }
